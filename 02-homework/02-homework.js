abc3271ce9283dd4e1ec944d1b5e6f136ce18797
AnnaRudka <anna.khomulyak@gmail.com>
2021-06-08 17:27:31 +0300

    Changes added

--- 02-homework/02-homework.js
+++ 02-homework/02-homework.js
@@ -3,42 +3,41 @@
 
 // Додаємо функцію, яка реагує на подію
 
-document.querySelector("form").addEventListener("submit", (event) => {
+const form = document.querySelector("form");
+const output = document.querySelector(".output");
+
+form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // Отримуємо дані від користувача з форми
-  const form = document.querySelector("form");
-  const startValue = +form.elements.start.value;
-  const endValue = +form.elements.end.value;
+  const firstNumber = +form.elements.start.value;
+  const secondNumber = +form.elements.end.value;
   const onlyOddNumbers = form.elements.checkbox.checked;
   let sum = 0;
 
   // Перевіряємо чи отримані дані є коректними
   if (
-    startValue >= endValue ||
-    startValue === "" ||
-    isNaN(startValue) ||
-    !Number.isInteger(startValue) ||
-    endValue === "" ||
-    isNaN(endValue) ||
-    !Number.isInteger(endValue)
+    firstNumber === "" ||
+    isNaN(firstNumber) ||
+    !Number.isInteger(firstNumber) ||
+    secondNumber === "" ||
+    isNaN(secondNumber) ||
+    !Number.isInteger(secondNumber)
   ) {
-    alert(
-      "Будь ласка введіть цілі числа і початкове значення більше за кінцеве"
-    );
+    alert("Будь ласка введіть цілі числа");
+    output.innerHTML = "Суму чисел неможливо розрахувати!";
   } else {
+    const startValue = Math.min(firstNumber, secondNumber);
+    const endValue = Math.max(firstNumber, secondNumber);
+
     for (let i = startValue; i <= endValue; i++) {
       if (onlyOddNumbers && i % 2 === 0) {
         continue; // Пропускаємо парні числа, якщо користувач це вказав
       }
       sum += i; // Розраховуємо суму чисел в діапазоні
     }
+    // Виводимо результат користувачу
+    output.innerHTML = `Сума чисел в діапазоні:  ${sum}`;
   }
-
-  // Виводимо результат користувачу
-  document.querySelector(
-    ".output"
-  ).innerHTML = `Сума чисел в діапазоні від  ${startValue} до ${endValue}:  ${sum}`;
-
   form.reset(); // Очищаємо форму
 });
