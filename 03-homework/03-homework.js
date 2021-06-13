26fbb1c2bd0cffc97cda7d2e07c0fd86decbdee5
AnnaRudka <anna.khomulyak@gmail.com>
2021-06-13 16:10:51 +0300

    Changes added

--- 03-homework/03-homework.js
+++ 03-homework/03-homework.js
@@ -2,26 +2,20 @@
 
 const output01 = document.querySelector(".output01");
 
-document.querySelector(".btn1").addEventListener("click", (event) => {
+document.querySelector(".btn1").addEventListener("click", () => {
   const number = Math.abs(+document.querySelector("#input01").value); // Отримаємо число від користувача (не враховуємо "-")
-  if (number === "" || isNaN(number)) {
-    // Перевіряємо чи число валідне
-    alert("Будь ласка введіть число");
-  } else {
-    const arrayOfDigits = number.toString().replace(".", "").split(""); // Створюємо масив з цифр числа
-    const maxDigit = Math.max(...arrayOfDigits); // Шукаємо максимальне значеня в масиві
-    output01.innerHTML = `Найбільша цифра в числі:  ${maxDigit}`;
-  }
+  const arrayOfDigits = number.toString().replace(".", "").split(""); // Створюємо масив з цифр числа
+  const maxDigit = Math.max(...arrayOfDigits); // Шукаємо максимальне значеня в масиві
+  output01.innerHTML = `Найбільша цифра в числі:  ${maxDigit}`;
 });
 
 // Створити функцію, яка визначає ступінь числа. Не використовуючи Math.pow та **. Використовуйте цикл
 
 const output02 = document.querySelector(".output02");
 
-document.querySelector(".btn2").addEventListener("click", (event) => {
-  const number = +document.querySelector("#input02-number").value; // Отримаємо число і значення степеня від користувача
-  const degree = +document.querySelector("#input02-degree").value;
-  let result = 1;
+document.querySelector(".btn2").addEventListener("click", () => {
+  const number = +document.querySelector("#input02-number").value;
+  const degree = +document.querySelector("#input02-degree").value; // Отримаємо число і значення степеня від користувача
   if (
     number === "" || // Перевіряємо на валідність
     isNaN(number) ||
@@ -29,8 +23,9 @@ document.querySelector(".btn2").addEventListener("click", (event) => {
     degree === "" ||
     isNaN(degree)
   ) {
-    alert("Будь ласка введіть числа (степінь - ціле число)");
+    output02.innerHTML = "Будь ласка введіть числа (степінь - ціле число)";
   } else {
+    let result = 1;
     for (let i = 1; i <= Math.abs(degree); i++) {
       //Розрахунок результату у випадках додатнього і від'ємного степеня
       if (degree > 0) {
@@ -46,7 +41,7 @@ document.querySelector(".btn2").addEventListener("click", (event) => {
 // Створити функцію, яка форматує ім'я, роблячи першу букву великою.
 const output03 = document.querySelector(".output03");
 
-document.querySelector(".btn3").addEventListener("click", (event) => {
+document.querySelector(".btn3").addEventListener("click", () => {
   const incorrectName = document.querySelector("#input03").value.toLowerCase();
   let correctName = incorrectName[0].toUpperCase() + incorrectName.slice(1);
   output03.innerHTML = `Правильний формат імені: ${correctName}`;
@@ -56,7 +51,7 @@ document.querySelector(".btn3").addEventListener("click", (event) => {
 
 const output04 = document.querySelector(".output04");
 
-document.querySelector(".btn4").addEventListener("click", (event) => {
+document.querySelector(".btn4").addEventListener("click", () => {
   const totalIncome = Math.abs(+document.querySelector("#input04").value);
   const taxValue = 19.5;
   const salary = ((totalIncome * (100 - taxValue)) / 100).toFixed(2);
@@ -67,18 +62,18 @@ document.querySelector(".btn4").addEventListener("click", (event) => {
 
 const output05 = document.querySelector(".output05");
 
-document.querySelector(".btn5").addEventListener("click", (event) => {
+document.querySelector(".btn5").addEventListener("click", () => {
   const firstNumber = +document.querySelector("#input05-first").value; // Отримаємо діапазон від користувача
   const secondNumber = +document.querySelector("#input05-second").value;
-  let randomValue = 0;
   if (
     firstNumber === "" || // Перевіряємо на валідність
     isNaN(firstNumber) ||
     secondNumber === "" ||
     isNaN(secondNumber)
   ) {
-    alert("Будь ласка введіть числа");
+    output05.innerHTML = "Будь ласка введіть числа";
   } else {
+    let randomValue = 0;
     const startValue = Math.min(firstNumber, secondNumber); // Сортуємо значення початку і кінця діапазону
     const endValue = Math.max(firstNumber, secondNumber);
     randomValue = Math.trunc(
@@ -92,9 +87,9 @@ document.querySelector(".btn5").addEventListener("click", (event) => {
 
 const output06 = document.querySelector(".output06");
 
-document.querySelector(".btn6").addEventListener("click", (event) => {
-  const word = document.querySelector("#input06-first").value; // Отримаємо дані від користувача
-  const letter = document.querySelector("#input06-second").value;
+document.querySelector(".btn6").addEventListener("click", () => {
+  const word = document.querySelector("#input06-first").toLowerCase(); // Отримаємо дані від користувача
+  const letter = document.querySelector("#input06-second").toLowerCase();
   let quantity = word.split(letter).length - 1;
   output06.innerHTML = `Кількість повторів літери ${letter} в слові ${word}: ${quantity}`;
 });
@@ -103,7 +98,7 @@ document.querySelector(".btn6").addEventListener("click", (event) => {
 
 const output07 = document.querySelector(".output07");
 
-document.querySelector(".btn7").addEventListener("click", (event) => {
+document.querySelector(".btn7").addEventListener("click", () => {
   const amount = document.querySelector("#input07").value.toLowerCase(); // Отримаємо дані від користувача
   const exchangeRate = 27;
   let amountAfterExchange = 0;
@@ -116,7 +111,7 @@ document.querySelector(".btn7").addEventListener("click", (event) => {
     amountAfterExchange = (amount.replace("$", "") * exchangeRate).toFixed(2);
     currency = "UAH";
   } else {
-    alert("Будь ласка введіть суму у UAH або $");
+    output07.innerHTML = "Будь ласка введіть суму у UAH або $";
   }
   output07.innerHTML = `Ви отримаєте: ${amountAfterExchange} ${currency}`;
 });
@@ -125,8 +120,8 @@ document.querySelector(".btn7").addEventListener("click", (event) => {
 
 const output08 = document.querySelector(".output08");
 
-document.querySelector(".btn8").addEventListener("click", (event) => {
-  const quantity = document.querySelector("#input08").value; // Отримаємо дані від користувача
+document.querySelector(".btn8").addEventListener("click", (quantity = 8) => {
+  quantity = document.querySelector("#input08").value; // Отримаємо дані від користувача
   let password = "";
   for (let i = 0; i < quantity; i++) {
     let number = Math.floor(Math.random() * 10); // Генеруємо випадкове число від 1 до 9
@@ -139,10 +134,10 @@ document.querySelector(".btn8").addEventListener("click", (event) => {
 
 const output09 = document.querySelector(".output09");
 
-document.querySelector(".btn9").addEventListener("click", (event) => {
-  const sentence = document.querySelector("#input09-first").value; // Отримаємо дані від користувача
-  const letter = document.querySelector("#input09-second").value;
-  let newSentence = sentence.replaceAll(letter, ""); // Робимо заміну символа на порожній рядок
+document.querySelector(".btn9").addEventListener("click", () => {
+  const sentence = document.querySelector("#input09-first").value.toLowerCase(); // Отримаємо дані від користувача
+  const letter = document.querySelector("#input09-second").value.toLowerCase();
+  const newSentence = sentence.replaceAll(letter, ""); // Робимо заміну символа на порожній рядок
   output09.innerHTML = `Речення без букви ${letter}: ${newSentence}`;
 });
 
@@ -150,12 +145,12 @@ document.querySelector(".btn9").addEventListener("click", (event) => {
 
 const output10 = document.querySelector(".output10");
 
-document.querySelector(".btn10").addEventListener("click", (event) => {
+document.querySelector(".btn10").addEventListener("click", () => {
   const word = document.querySelector("#input10").value.replaceAll(" ", ""); // Отримаємо дані від користувача
   // Розвертаємо слово в зворотньому порядку
   const reverseword = word.toLowerCase().split("").reverse().join("");
   // Перевіряємо тотожність з оригіналом
-  if (reverseword == word.toLowerCase()) {
+  if (reverseword === word.toLowerCase()) {
     output10.innerHTML = `Це слово є паліндромом`;
   } else {
     output10.innerHTML = `Це слово не є паліндромом`;
@@ -166,12 +161,15 @@ document.querySelector(".btn10").addEventListener("click", (event) => {
 
 const output11 = document.querySelector(".output11");
 
-document.querySelector(".btn11").addEventListener("click", (event) => {
+document.querySelector(".btn11").addEventListener("click", () => {
   const sentence = document.querySelector("#input11").value; // Отримаємо дані від користувача
   let newSentence = "";
   for (let i = 0; i < sentence.length; i++) {
     // Перевіряємо в циклі чи індекс символу = останньому індексу цього символу
-    if (sentence.indexOf(sentence[i]) == sentence.lastIndexOf(sentence[i])) {
+    if (
+      sentence.toLowerCase().indexOf(sentence[i]) ===
+      sentence.lastIndexOf(sentence[i].toLowerCase())
+    ) {
       newSentence += sentence[i];
     }
   }
